634576290228bcbdb53ae246d35be3a15785bfcd
Maxwel Mazur <52722027+MaxwelMazur@users.noreply.github.com>
2023-01-11 17:58:21 -0300

    Update tablecli.go

--- tablecli.go
+++ tablecli.go
@@ -8,90 +8,19 @@ import (
 	"unicode/utf8"
 )
 
-// These are the default properties for all Tables created from this package
-// and can be modified.
 var (
-	// DefaultPadding specifies the number of spaces between columns in a table.
 	DefaultPadding = 2
-
-	// DefaultWriter specifies the output io.Writer for the Table.Print method.
 	DefaultWriter io.Writer = os.Stdout
-
-	// DefaultHeaderFormatter specifies the default Formatter for the table Header.
 	DefaultHeaderFormatter Formatter
-
-	// DefaultFirstColumnFormatter specifies the default Formatter for the first column cells.
 	DefaultFirstColumnFormatter Formatter
-
-	// DefaultWidthFunc specifies the default WidthFunc for calculating column Widths
 	DefaultWidthFunc WidthFunc = utf8.RuneCountInString
-
-    WidthPersist []int
+   	WidthPersist []int
 )
 
-// Formatter functions expose a fmt.Sprintf signature that can be used to modify
-// the display of the text in either the Header or first column of a Table.
-// The formatter should not change the width of original text as printed since
-// column Widths are calculated pre-formatting (though this issue can be mitigated
-// with increased padding).
-//
-//   tbl.WithHeaderFormatter(func(format string, vals ...interface{}) string {
-//     return strings.ToUpper(fmt.Sprintf(format, vals...))
-//   })
-//
-// A good use case for formatters is to use ANSI escape codes to color the cells
-// for a nicer interface. The package color (https://github.com/fatih/color) makes
-// it easy to generate these automatically: http://godoc.org/github.com/fatih/color#Color.SprintfFunc
 type Formatter func(string, ...interface{}) string
 
-// A WidthFunc calculates the width of a string. By default, the number of runes
-// is used but this may not be appropriate for certain character sets. The
-// package runewidth (https://github.com/mattn/go-runewidth) could be used to
-// accomodate multi-cell characters (such as emoji or CJK characters).
 type WidthFunc func(string) int
 
-// Table describes the interface for building up a tabular representation of data.
-// It exposes fluent/chainable methods for convenient table building.
-//
-// WithHeaderFormatter and WithFirstColumnFormatter sets the Formatter for the
-// Header and first column, respectively. If nil is passed in (the default), no
-// formatting will be applied.
-//
-//   New("foo", "bar").WithFirstColumnFormatter(func(f string, v ...interface{}) string {
-//     return strings.ToUpper(fmt.Sprintf(f, v...))
-//   })
-//
-// WithPadding specifies the minimum padding between cells in a row and defaults
-// to DefaultPadding. Padding values less than or equal to zero apply no extra
-// padding between the columns.
-//
-//   New("foo", "bar").WithPadding(3)
-//
-// WithWriter modifies the writer which Print outputs to, defaulting to DefaultWriter
-// when instantiated. If nil is passed, os.Stdout will be used.
-//
-//   New("foo", "bar").WithWriter(os.Stderr)
-//
-// WithWidthFunc sets the function used to calculate the width of the string in
-// a column. By default, the number of utf8 runes in the string is used.
-//
-// AddRow adds another row of data to the table. Any values can be passed in and
-// will be output as its string representation as described in the fmt standard
-// package. Rows can have less cells than the total number of columns in the table;
-// subsequent cells will be rendered empty. Rows with more cells than the total
-// number of columns will be truncated. References to the data are not held, so
-// the passed in values can be modified without affecting the table's output.
-//
-//   New("foo", "bar").AddRow("fizz", "buzz").AddRow(time.Now()).AddRow(1, 2, 3).Print()
-//   // Output:
-//   // foo                              bar
-//   // fizz                             buzz
-//   // 2006-01-02 15:04:05.0 -0700 MST
-//   // 1                                2
-//
-// Print writes the string representation of the table to the provided writer.
-// Print can be called multiple times, even after subsequent mutations of the
-// provided data. The output is always preceded and followed by a new line.
 type Table interface {
 	WithHeaderFormatter(f Formatter) Table
 	WithFirstColumnFormatter(f Formatter) Table
@@ -102,20 +31,17 @@ type Table interface {
 	AddRow(vals ...interface{}) Table
 	SetRows(Rows [][]string) Table
 	Print()
-    CalculateWidths([]string)
-    GetHeader() []string
-    GetRows() [][]string
-    PrintHeader(format string)
-    PrintRow(format string, row []string)
+	CalculateWidths([]string)
+   	GetHeader() []string
+   	GetRows() [][]string
+    	PrintHeader(format string)
+    	PrintRow(format string, row []string)
 }
 
-// New creates a Table instance with the specified Header(s) provided. The number
-// of columns is fixed at this point to len(columnHeaders) and the defined defaults
-// are set on the instance.
 func New(columnHeaders ...interface{}) Table {
 	t := table{
-        Header: make([]string, len(columnHeaders)),
-    }
+        	Header: make([]string, len(columnHeaders)),
+    	}
 
 	t.WithPadding(DefaultPadding)
 	t.WithWriter(DefaultWriter)
