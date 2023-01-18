e618a5620872db321a7f2b9c47c6c1af5cac31b8
golpher96 <maxwelbm@hotmail.com>
2023-01-18 13:25:59 -0300

    ref: rename name gif

--- tablecli.go
+++ tablecli.go
@@ -1,20 +1,20 @@
 package tablecli
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"strings"
-	"unicode/utf8"
+    "fmt"
+    "io"
+    "os"
+    "strings"
+    "unicode/utf8"
 )
 
 var (
-	DefaultPadding = 2
-	DefaultWriter io.Writer = os.Stdout
-	DefaultHeaderFormatter Formatter
-	DefaultFirstColumnFormatter Formatter
-	DefaultWidthFunc WidthFunc = utf8.RuneCountInString
-   	WidthPersist []int
+    DefaultPadding = 2
+    DefaultWriter io.Writer = os.Stdout
+    DefaultHeaderFormatter Formatter
+    DefaultFirstColumnFormatter Formatter
+    DefaultWidthFunc WidthFunc = utf8.RuneCountInString
+    WidthPersist []int
 )
 
 type Formatter func(string, ...interface{}) string
@@ -22,158 +22,157 @@ type Formatter func(string, ...interface{}) string
 type WidthFunc func(string) int
 
 type Table interface {
-	WithHeaderFormatter(f Formatter) Table
-	WithFirstColumnFormatter(f Formatter) Table
-	WithPadding(p int) Table
-	WithWriter(w io.Writer) Table
-	WithWidthFunc(f WidthFunc) Table
-
-	AddRow(vals ...interface{}) Table
-	SetRows(Rows [][]string) Table
-	Print()
-	CalculateWidths([]string)
-   	GetHeader() []string
-   	GetRows() [][]string
-    	PrintHeader(format string)
-    	PrintRow(format string, row []string)
+    WithHeaderFormatter(f Formatter) Table
+    WithFirstColumnFormatter(f Formatter) Table
+    WithPadding(p int) Table
+    WithWriter(w io.Writer) Table
+    WithWidthFunc(f WidthFunc) Table
+    AddRow(vals ...interface{}) Table
+    SetRows(Rows [][]string) Table
+    Print()
+    CalculateWidths([]string)
+    GetHeader() []string
+    GetRows() [][]string
+    PrintHeader(format string)
+    PrintRow(format string, row []string)
 }
 
 func New(columnHeaders ...interface{}) Table {
-	t := table{
-        	Header: make([]string, len(columnHeaders)),
-    	}
+    t := table{
+        Header: make([]string, len(columnHeaders)),
+    }
 
-	t.WithPadding(DefaultPadding)
-	t.WithWriter(DefaultWriter)
-	t.WithHeaderFormatter(DefaultHeaderFormatter)
-	t.WithFirstColumnFormatter(DefaultFirstColumnFormatter)
-	t.WithWidthFunc(DefaultWidthFunc)
+    t.WithPadding(DefaultPadding)
+    t.WithWriter(DefaultWriter)
+    t.WithHeaderFormatter(DefaultHeaderFormatter)
+    t.WithFirstColumnFormatter(DefaultFirstColumnFormatter)
+    t.WithWidthFunc(DefaultWidthFunc)
 
-	for i, col := range columnHeaders {
-		t.Header[i] = fmt.Sprint(col)
-	}
+    for i, col := range columnHeaders {
+        t.Header[i] = fmt.Sprint(col)
+    }
 
-	return &t
+    return &t
 }
 
 type table struct {
-	FirstColumnFormatter Formatter
-	HeaderFormatter      Formatter
-	Padding              int
-	Writer               io.Writer
-	Width                WidthFunc
+    FirstColumnFormatter Formatter
+    HeaderFormatter      Formatter
+    Padding              int
+    Writer               io.Writer
+    Width                WidthFunc
 
-	Header []string
-	Rows   [][]string
-	Widths []int
+    Header []string
+    Rows   [][]string
+    Widths []int
 }
 
 
 func (t *table) GetRows() [][]string {
-	return t.Rows
+    return t.Rows
 }
 
 func (t *table) GetHeader() []string {
-	return t.Header
+    return t.Header
 }
 
 func (t *table) WithHeaderFormatter(f Formatter) Table {
-	t.HeaderFormatter = f
-	return t
+    t.HeaderFormatter = f
+    return t
 }
 
 func (t *table) WithFirstColumnFormatter(f Formatter) Table {
-	t.FirstColumnFormatter = f
-	return t
+    t.FirstColumnFormatter = f
+    return t
 }
 
 func (t *table) WithPadding(p int) Table {
-	if p < 0 {
-		p = 0
-	}
+    if p < 0 {
+        p = 0
+    }
 
-	t.Padding = p
-	return t
+    t.Padding = p
+    return t
 }
 
 func (t *table) WithWriter(w io.Writer) Table {
-	if w == nil {
-		w = os.Stdout
-	}
+    if w == nil {
+        w = os.Stdout
+    }
 
-	t.Writer = w
-	return t
+    t.Writer = w
+    return t
 }
 
 func (t *table) WithWidthFunc(f WidthFunc) Table {
-	t.Width = f
-	return t
+    t.Width = f
+    return t
 }
 
 func (t *table) AddRow(vals ...interface{}) Table {
-	maxNumNewlines := 0
-	for _, val := range vals {
-		maxNumNewlines = max(strings.Count(fmt.Sprint(val), "\n"), maxNumNewlines)
-	}
-	for i := 0; i <= maxNumNewlines; i++ {
-		row := make([]string, len(t.Header))
-		for j, val := range vals {
-			if j >= len(t.Header) {
-				break
-			}
-			v := strings.Split(fmt.Sprint(val), "\n")
-			row[j] = safeOffset(v, i)
-		}
-		t.Rows = append(t.Rows, row)
-	}
+    maxNumNewlines := 0
+    for _, val := range vals {
+        maxNumNewlines = max(strings.Count(fmt.Sprint(val), "\n"), maxNumNewlines)
+    }
+    for i := 0; i <= maxNumNewlines; i++ {
+        row := make([]string, len(t.Header))
+        for j, val := range vals {
+            if j >= len(t.Header) {
+                break
+            }
+            v := strings.Split(fmt.Sprint(val), "\n")
+            row[j] = safeOffset(v, i)
+        }
+        t.Rows = append(t.Rows, row)
+    }
 
-	return t
+    return t
 }
 
 func (t *table) SetRows(Rows [][]string) Table {
-	t.Rows = [][]string{}
-	headerLength := len(t.Header)
+    t.Rows = [][]string{}
+    headerLength := len(t.Header)
 
-	for _, row := range Rows {
-		if len(row) > headerLength {
-			t.Rows = append(t.Rows, row[:headerLength])
-		} else {
-			t.Rows = append(t.Rows, row)
-		}
-	}
+    for _, row := range Rows {
+        if len(row) > headerLength {
+            t.Rows = append(t.Rows, row[:headerLength])
+        } else {
+            t.Rows = append(t.Rows, row)
+        }
+    }
 
-	return t
+    return t
 }
 
 func (t *table) Print() {
-	format := strings.Repeat("%s", len(t.Header)) + "\n"
+    format := strings.Repeat("%s", len(t.Header)) + "\n"
     fmt.Println("format ", format)
-	t.CalculateWidths([]string{})
+    t.CalculateWidths([]string{})
 
-	t.PrintHeader(format)
-	for _, row := range t.Rows {
-		t.PrintRow(format, row)
-	}
+    t.PrintHeader(format)
+    for _, row := range t.Rows {
+        t.PrintRow(format, row)
+    }
 }
 
 func (t *table) PrintHeader(format string) {
-	vals := t.applyWidths(t.Header, t.Widths)
-	if t.HeaderFormatter != nil {
-		txt := t.HeaderFormatter(format, vals...)
-		fmt.Fprint(t.Writer, txt)
-	} else {
-		fmt.Fprintf(t.Writer, format, vals...)
-	}
+    vals := t.applyWidths(t.Header, t.Widths)
+    if t.HeaderFormatter != nil {
+        txt := t.HeaderFormatter(format, vals...)
+        fmt.Fprint(t.Writer, txt)
+    } else {
+        fmt.Fprintf(t.Writer, format, vals...)
+    }
 }
 
 func (t *table) PrintRow(format string, row []string) {
-	vals := t.applyWidths(row, t.Widths)
+    vals := t.applyWidths(row, t.Widths)
 
-	if t.FirstColumnFormatter != nil {
-		vals[0] = t.FirstColumnFormatter("%s", vals[0])
-	}
+    if t.FirstColumnFormatter != nil {
+        vals[0] = t.FirstColumnFormatter("%s", vals[0])
+    }
 
-	fmt.Fprintf(t.Writer, format, vals...)
+    fmt.Fprintf(t.Writer, format, vals...)
 }
 
 func (t *table) CalculateWidths(h []string) {
@@ -181,20 +180,20 @@ func (t *table) CalculateWidths(h []string) {
         h = t.Header
     }
 
-	t.Widths = make([]int, len(h))
-	for _, row := range t.Rows {
-		for i, v := range row {
-			if w := t.Width(v) + t.Padding; w > t.Widths[i] {
-				t.Widths[i] = w
-			}
-		}
-	}
+    t.Widths = make([]int, len(h))
+    for _, row := range t.Rows {
+        for i, v := range row {
+            if w := t.Width(v) + t.Padding; w > t.Widths[i] {
+                t.Widths[i] = w
+            }
+        }
+    }
 
-	for i, v := range t.Header {
-		if w := t.Width(v) + t.Padding; w > t.Widths[i] {
-			t.Widths[i] = w
-		}
-	}
+    for i, v := range t.Header {
+        if w := t.Width(v) + t.Padding; w > t.Widths[i] {
+            t.Widths[i] = w
+        }
+    }
 
     if len(WidthPersist) > 0 {
         for i:=0; i<len(t.Widths); i++ {
@@ -208,31 +207,31 @@ func (t *table) CalculateWidths(h []string) {
 }
 
 func (t *table) applyWidths(row []string, Widths []int) []interface{} {
-	out := make([]interface{}, len(row))
-	for i, s := range row {
-		out[i] = s + t.lenOffset(s, Widths[i])
-	}
-	return out
+    out := make([]interface{}, len(row))
+    for i, s := range row {
+        out[i] = s + t.lenOffset(s, Widths[i])
+    }
+    return out
 }
 
 func (t *table) lenOffset(s string, w int) string {
-	l := w - t.Width(s)
-	if l <= 0 {
-		return ""
-	}
-	return strings.Repeat(" ", l)
+    l := w - t.Width(s)
+    if l <= 0 {
+        return ""
+    }
+    return strings.Repeat(" ", l)
 }
 
 func max(i1, i2 int) int {
-	if i1 > i2 {
-		return i1
-	}
-	return i2
+    if i1 > i2 {
+        return i1
+    }
+    return i2
 }
 
 func safeOffset(sarr []string, idx int) string {
-	if idx >= len(sarr) {
-		return ""
-	}
-	return sarr[idx]
+    if idx >= len(sarr) {
+        return ""
+    }
+    return sarr[idx]
 }
